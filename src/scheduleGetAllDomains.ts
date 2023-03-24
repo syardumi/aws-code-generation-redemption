ca449fb0d7ca9c2ebae6c31ba76f948b205cbbc8
Steve Yardumian <steveyardumian@gmail.com>
2023-03-23 20:50:13 -0400

    working on the sqs handling

--- src/scheduleGetAllDomains.ts
+++ src/scheduleGetAllDomains.ts
@@ -2,7 +2,38 @@ import { Handler } from 'aws-lambda'
 import AWS from 'aws-sdk'
 
 export const ddb = new AWS.DynamoDB.DocumentClient()
+export const sqs = new AWS.SQS()
 
 export const fire: Handler = async (event, _context, _callback) => {
-  // TODO: get all domains and send to SQS
+  let result, ExclusiveStartKey
+  try {
+    do {
+      result = await ddb
+        .scan({
+          TableName: process.env.DOMAIN_TABLE_NAME,
+          ExclusiveStartKey,
+          Limit: 100
+        })
+        .promise()
+
+      await Promise.all(
+        result.Items.map(async (Item) => {
+          const msg = {
+            MessageBody: JSON.stringify({ domain: Item.code_domain }),
+            QueueUrl: process.env.CODE_EXPIRATION_QUEUE_URL
+          }
+          await sqs
+            .sendMessage(msg)
+            .promise()
+            .catch((err) => {
+              console.error('Failed to send message to SQS', { msg }, err)
+            })
+        })
+      )
+
+      ExclusiveStartKey = result.LastEvaluatedKey
+    } while (result.Items.length && result.LastEvaluatedKey)
+  } catch (e) {
+    console.error('Schedule Get All Domains Error', e)
+  }
 }
